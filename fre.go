c4547ef4e6dcc550446f66239a0f1dec6b6339bb
mtso <mtso@tuta.io>
2017-01-01 00:12:24 -0800

    Create fre.go for Flesch Reading-Ease test

---
+++ fre.go
@@ -0,0 +1,10 @@
+package read
+
+// Fre scores the Flesch Reading-Ease.
+// See https://en.wikipedia.org/wiki/Flesch–Kincaid_readability_tests#Flesch_reading_ease.
+func Fre(text string) float64 {
+	sylCnt := float64(CntSyls(text))
+	wordCnt := float64(CntWords(text))
+	sentCnt := float64(CntSents(text))	
+	return 206.835 - 1.015*(wordCnt/sentCnt) - 84.6*(sylCnt/wordCnt)
+}
